0fda02180f5e9e626a187c8dcce0d9ef2e2df3c2
Etson <etsond@ufl.edu>
2024-04-22 17:32:47 -0400

    first program

---
+++ hellos.js
@@ -0,0 +1,12 @@
+`use strict`;
+
+function fistProgram(){
+    var message= 'Hello World'
+    var name = 'Frank'
+
+    console.log(message + ' '+  name + ' we are excited to see you');
+}
+
+fistProgram()
+
+
